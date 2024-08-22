ae781f937dde120d42a5ca1b3de0c394718d90c1
liuyaping <liuyaping@ldrobot.com>
2024-08-22 14:13:16 +0800

    fix watch_mem

--- src/jarvis/configuration/watch_mem.sh
+++ src/jarvis/configuration/watch_mem.sh
@@ -23,8 +23,8 @@ do
     free -h |grep Mem | awk -F ' ' ' {print $3}'
     cat  /proc/$pid/status|grep -e VmRSS
     cat  /proc/$pid/status|grep -e VmRSS >>proc_memlog.txt
-    top -n 1 |grep jarvis |head -1 
-    top -n 1 |grep jarvis |head -1 >> proc_memlog.txt
+    top -n 1 |grep mpslam |head -1 
+    top -n 1 |grep mpslam |head -1 >> proc_memlog.txt
     #cpu=`top -n 1 |grep jarvis |head -1 |awk '{ssd=NF-1} {print $ssd}'`
     #cpu=`top -n 1 -p $pid|tail -3|head -1|awk '{ssd=NF-4} {print $ssd}'`    #获取cpu占用
     echo $blank
