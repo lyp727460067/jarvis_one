f6b2f4d50d4e273aaeefd610f0e18f3845f22e38
liuyaping <liuyaping@ldrobot.com>
2024-12-06 10:04:47 +0800

    add grid map

---
+++ src/jarvis/jarvis/grid_map/2d/probability_grid_range_data_inserter_2d.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
+#define CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
+
+#include <utility>
+#include <vector>
+
+#include "jarvis/common/port.h"
+#include "jarvis/grid_map/2d/probability_grid.h"
+#include "jarvis/grid_map/2d/xy_index.h"
+#include "jarvis/grid_map/2d/point_cloud.h"
+#include "jarvis/grid_map/2d/range_data.h"
+
+namespace jarvis {
+namespace grid_map {
+
+struct ProbabilityGridRangeDataInserterOptions2D {
+  float hit_probability = 0.55;
+  float miss_probability = 0.4;
+  bool insert_free_space = true;
+};
+class ProbabilityGridRangeDataInserter2D {
+ public:
+  explicit ProbabilityGridRangeDataInserter2D(
+      const ProbabilityGridRangeDataInserterOptions2D& options);
+
+  ProbabilityGridRangeDataInserter2D(
+      const ProbabilityGridRangeDataInserter2D&) = delete;
+  ProbabilityGridRangeDataInserter2D& operator=(
+      const ProbabilityGridRangeDataInserter2D&) = delete;
+
+  // Inserts 'range_data' into 'probability_grid'.
+  virtual void Insert(const sensor::RangeData& range_data,
+                      Grid2D* grid) const ;
+
+ private:
+  const ProbabilityGridRangeDataInserterOptions2D options_;
+  const std::vector<uint16> hit_table_;
+  const std::vector<uint16> miss_table_;
+};
+
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
