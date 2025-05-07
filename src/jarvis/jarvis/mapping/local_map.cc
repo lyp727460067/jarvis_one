ff41d595492d621bab8f3b49d575c68bdc47ee4d
lyp727460067 <727460607@qq.com>
2025-05-07 14:08:00 +0800

    test

--- src/jarvis/jarvis/mapping/local_map.cc
+++ src/jarvis/jarvis/mapping/local_map.cc
@@ -349,16 +349,16 @@ void LocalMap::TrimRedundancy() {
     data_culling_->CullingMapSimilarMap(c_id);
     // data_culling_->KeyFrameCulling(c_id);
   }
-  // std::set<MapPointId> less_map_points_ids;
-  // for (const auto &map_point_id : data_.map_points) {
-  //   if (data_.covisibility.GetMapObservations(map_point_id.id).size() < 2) {
-  //     less_map_points_ids.insert(map_point_id.id);
-  //   }
-  // }
-  // for (auto &id : less_map_points_ids) {
-  //   data_.covisibility.TrimMapPoint(id);
-  //   data_.map_points.Trim(id);
-  // }
+  std::set<MapPointId> less_map_points_ids;
+  for (const auto &map_point_id : data_.map_points) {
+    if (data_.covisibility.GetMapObservations(map_point_id.id).size() < 2) {
+      less_map_points_ids.insert(map_point_id.id);
+    }
+  }
+  for (auto &id : less_map_points_ids) {
+    data_.covisibility.TrimMapPoint(id);
+    data_.map_points.Trim(id);
+  }
 }
 //
 //
